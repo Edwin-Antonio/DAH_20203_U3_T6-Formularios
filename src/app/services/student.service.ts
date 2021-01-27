e6943fc6b2dded0d596d930670e1f1d497e66ab6
Edwin-Antonio <edanarellanoma@ittepic.edu.mx>
2021-01-27 02:45:54 -0800

    [V0.1.0]

---
+++ src/app/services/student.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import { Student } from '../models/student';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private students: Student[] = new Array();
+
+  constructor() {
+    this.students.push({
+      name: 'Edwin Antonio Arellano Mata',
+      controlnumer: '16400882',
+      active: true
+    });
+    this.students.push({
+      name: 'Francisco Javier Flores Herrera',
+      controlnumer: '16400999',
+      active: true
+    });
+  }
+
+  getStudents(): Student[]{
+    return this.students;
+  }
+
+  changeStatus(position: number): void{
+    this.students[position].active = !this.students[position].active;
+  }
+
+  deleteStudent(position: number): void{
+    this.students.splice(position, 1);
+  }
+
+  newStudent(student: Student): void{
+    this.students.push(student);
+  }
+}
+
